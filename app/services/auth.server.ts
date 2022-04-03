a1ce276ece32d68c8a312edfb45df0da6994c754
Hyeseong Kim <hey@hyeseong.kim>
2022-04-03 19:06:28 +0900

    update callback url

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -9,9 +9,7 @@ authenticator.use(
   new GitHubStrategy<User>({
     clientID: process.env.GITHUB_APP_CLIENT_ID!,
     clientSecret: process.env.GITHUB_APP_CLIENT_SECRET!,
-    callbackURL: process.env.NODE_ENV === 'production'
-      ? 'https://poc-webapp-provider.cometkim.pages.dev/auth/github/callback'
-      : 'http://localhost:8788/auth/github/callback',
+    callbackURL: process.env.GITHUB_APP_CALLBACK_URL || 'http://localhost:8788/auth/github/callback',
   }, async ({ profile }) => {
     return {
       email: profile.emails[0].value,
